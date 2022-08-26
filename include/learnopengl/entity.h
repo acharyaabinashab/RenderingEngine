5c2ab7189d6d784275e09d6ce99f8c6a4e1b7207
acharyaabinashab <acharyaabinash1@gmail.com>
2022-08-27 01:30:09 +0545

    Changed entityName type

--- include/learnopengl/entity.h
+++ include/learnopengl/entity.h
@@ -395,7 +395,7 @@ public:
 	Model* pModel = nullptr;
 	std::unique_ptr<AABB> boundingVolume;
 
-	const char* entityName = "New Entity";
+	char entityName[128] = "New Element";
 
 	// constructor, expects a filepath to a 3D model.
 	Entity(Model& model) : pModel{ &model }
@@ -407,7 +407,7 @@ public:
 	// constructor, expects a filepath to a 3D model.
 	Entity(Model& model, const char* name) : pModel{ &model }
 	{
-		entityName = name;
+		strcpy(entityName, name);
 		boundingVolume = std::make_unique<AABB>(generateAABB(model));
 		//boundingVolume = std::make_unique<Sphere>(generateSphereBV(model));
 	}
